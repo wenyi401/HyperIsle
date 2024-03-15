0779f42bbfa8987c248ceb6f25d7d6bc89495374
hd <1839732296@qq.com>
2024-03-16 01:27:51 +0800

    01:27 kotlin blurDialog

--- app/src/main/java/art/luaj/hyperisle/ui/dialog/BlurDialogBuilder.kt
+++ app/src/main/java/art/luaj/hyperisle/ui/dialog/BlurDialogBuilder.kt
@@ -1,63 +1,50 @@
-package art.luaj.hyperisle.ui.dialog;
+package art.luaj.hyperisle.ui.dialog
 
-import android.content.Context;
-import android.os.Build;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+import android.content.Context
+import android.os.Build
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
+class BlurDialogBuilder : MaterialAlertDialogBuilder {
+    constructor(context: Context) : super(context)
 
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+    constructor(context: Context, overrideThemeResId: Int) : super(context, overrideThemeResId)
 
-import java.util.function.Consumer;
-
-public class BlurDialogBuilder extends MaterialAlertDialogBuilder {
-    public BlurDialogBuilder(@NonNull Context context) {
-        super(context);
+    override fun create(): AlertDialog {
+        val dialog = super.create()
+        addWindowBlurListener(dialog)
+        return dialog
     }
 
-    public BlurDialogBuilder(@NonNull Context context, int overrideThemeResId) {
-        super(context, overrideThemeResId);
-    }
-
-    @Override
-    public AlertDialog create() {
-        AlertDialog dialog = super.create();
-        setupWindowBlurListener(dialog);
-        return dialog;
-    }
-
-    private void setupWindowBlurListener(AlertDialog dialog) {
-        Window window = dialog.getWindow();
+    private fun addWindowBlurListener(dialog: AlertDialog) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            window.addFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND);
-            Consumer<Boolean> windowBlurEnabledListener = enabled -> updateWindowForBlurs(window, enabled);
-            window.getDecorView().addOnAttachStateChangeListener(
-                    new View.OnAttachStateChangeListener() {
-                        @Override
-                        public void onViewAttachedToWindow(View view) {
-                            window.getWindowManager().addCrossWindowBlurEnabledListener(
-                                    windowBlurEnabledListener);
-                        }
+            val window = dialog.window
+            window?.addFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND)
+            window?.decorView?.addOnAttachStateChangeListener(object :
+                View.OnAttachStateChangeListener {
+                override fun onViewAttachedToWindow(v: View) {
+                    window.windowManager?.addCrossWindowBlurEnabledListener { enabled ->
+                        updateWindowBlur(window, enabled)
+                    }
+                }
 
-                        @Override
-                        public void onViewDetachedFromWindow(View view) {
-                            window.getWindowManager().removeCrossWindowBlurEnabledListener(
-                                    windowBlurEnabledListener);
-                        }
-                    });
+                override fun onViewDetachedFromWindow(v: View) {
+                    window.windowManager?.removeCrossWindowBlurEnabledListener { enabled ->
+                        updateWindowBlur(window, enabled)
+                    }
+                }
+            })
         }
     }
 
-    private void updateWindowForBlurs(Window window, boolean blursEnabled) {
-        float mDimAmountWithBlur = 0.1f;
-        float mDimAmountNoBlur = 0.35f;
-        window.setDimAmount(blursEnabled ? mDimAmountWithBlur : mDimAmountNoBlur);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            window.getAttributes().setBlurBehindRadius(20);
-            window.setAttributes(window.getAttributes());
-        }
+    private fun updateWindowBlur(window: Window, enabled: Boolean) {
+        val mDimAmountWithBlur = 0.1f
+        val mDimAmountNoBlur = 0.35f
+        window.setDimAmount(if (enabled) mDimAmountWithBlur else mDimAmountNoBlur)
+        window.attributes.blurBehindRadius = 20
+        window.attributes = window.attributes
     }
 }
