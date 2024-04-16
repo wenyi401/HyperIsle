9b3213d04054fc82c2efc2df1419333f9d4f1708
wenyi401 <726985584@qq.com>
2024-04-16 21:24:29 +0800

    优化

--- app/src/main/java/art/luaj/hyperisle/Application.java
+++ app/src/main/java/art/luaj/hyperisle/Application.java
@@ -1,6 +1,24 @@
 package art.luaj.hyperisle;
 
+import android.content.SharedPreferences;
 import art.luaj.hyperisle.ext.BaseApplication;
+import art.luaj.hyperisle.ext.Config;
 
 public class Application extends BaseApplication {
+    private static Application instance;
+    private static SharedPreferences pref;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        pref = getSharedPreferences(Config.APP_DEFAULT_STORAGE, MODE_PRIVATE);
+    }
+
+    public static Application getInstance() {
+        return instance;
+    }
+
+    public static SharedPreferences getPreferences() {
+        return instance.pref;
+    }
 }
--- app/src/main/java/art/luaj/hyperisle/ui/base/BaseActivity.kt
+++ app/src/main/java/art/luaj/hyperisle/ui/base/BaseActivity.kt
@@ -1,21 +1,34 @@
 package art.luaj.hyperisle.ui.base
 
 import android.annotation.SuppressLint
+import android.content.res.Resources
+import android.graphics.Color
 import android.os.Bundle
 import android.util.TypedValue
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import art.luaj.hyperisle.R
 import art.luaj.hyperisle.ui.utils.isSystemInDarkMode
 import com.google.android.material.internal.EdgeToEdgeUtils
+import rikka.material.app.MaterialActivity
 
-open class BaseActivity : AppCompatActivity() {
-    @SuppressLint("WrongConstant", "RestrictedApi")
+open class BaseActivity : MaterialActivity() {
+
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.AppTheme)
         EdgeToEdgeUtils.applyEdgeToEdge(window, true)
-        initBar()
+        //initBar()
+    }
+
+    override fun onApplyThemeResource(theme: Resources.Theme, resid: Int, first: Boolean) {
+        theme.applyStyle(rikka.material.preference.R.style.ThemeOverlay_Rikka_Material3_Preference, true)
+    }
+
+    override fun onApplyTranslucentSystemBars() {
+        super.onApplyTranslucentSystemBars()
+        window.statusBarColor =  Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
     }
 
     private fun initBar() {
